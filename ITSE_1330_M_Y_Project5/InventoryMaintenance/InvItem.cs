3143c824c4de3161bf0ea0431a753b7855217820
megan-yiu <megan.n.yiu@gmail.com>
2021-02-05 17:20:56 -0600

    Add files via upload
    2016 Inventory Management - Expands Assignment so that user can enter and delete entries containing number, description, size from selection, and price

---
+++ ITSE_1330_M_Y_Project5/InventoryMaintenance/InvItem.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    public class InvItem
+    {
+        private int itemNo;
+        private string description;
+        private decimal price;
+
+        public InvItem()
+        {
+        }
+
+        public InvItem(int itemNo, string description, decimal price)
+        {
+            this.itemNo = itemNo;
+            this.description = description;
+            this.price = price;
+        }
+
+        public int ItemNo
+        {
+            get
+            {
+                return itemNo;
+            }
+            set
+            {
+                itemNo = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+
+        public virtual string GetDisplayText() => itemNo + "    " + description + " (" + price.ToString("c") + ")";
+    }
+
+    public class Plant : InvItem
+    {
+        private string size;
+        
+        public Plant()
+        {
+        }
+
+        public Plant(int itemNo, string size, string description, 
+            decimal price) : base(itemNo, description, price)
+        {
+            this.Size = size;
+        }
+
+        public string Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+
+        public override string GetDisplayText() =>
+            ItemNo + "    " + Size + " " + Description 
+            + " (" + Price.ToString("c") + ")";
+    }
+    public class Supply : InvItem
+    {
+        private string manufacturer;
+
+        public Supply()
+        {
+        }
+
+        public Supply(int itemNo,string manufacturer, string description, 
+            decimal price) : base(itemNo, description, price)
+        {
+            this.Manufacturer = manufacturer;
+        }
+
+        public string Manufacturer
+        {
+            get
+            {
+                return manufacturer;
+            }
+            set
+            {
+                manufacturer = value;
+            }
+        }
+
+        public override string GetDisplayText() =>
+            ItemNo + "    " + Manufacturer + " "+ Description
+            + " (" + Price.ToString("c") + ")";
+    }
+
+}
